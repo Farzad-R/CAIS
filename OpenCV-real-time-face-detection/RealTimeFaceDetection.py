d0d0af3a7dcb77ad8f40d848d8917dc486d867a8
DESKTOP-RLDOUPR\farza <farzad.workdesk@gmail.com>
2023-03-19 00:17:53 -0400

    update

--- OpenCV-real-time-face-detection/RealTimeFaceDetection.py
+++ OpenCV-real-time-face-detection/RealTimeFaceDetection.py
@@ -1,37 +1,29 @@
 """
 This code uses the Haar Cascade face detection algorithm to detect 
-faces in each frame of the video stream from the webcam. It then draws 
+faces in each frame of the video stream from the webcam and it draws 
 a rectangle around each detected face and displays the resulting image 
-in a window. The program will continue running until you press the 'q' 
-key to quit.
+in a window. 
+To quit, press 'q'.
 """
-
 import cv2
 
 # Load the pre-trained Haar Cascade face detection model
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-
 # Start capturing video from the default camera
 video_capture = cv2.VideoCapture(0)
-
 # Continuously read frames from the camera and detect faces
 while True:
     # Read a frame from the camera
     ret, frame = video_capture.read()
-
     # Convert the frame to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
     # Detect faces in the grayscale image
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-
     # Draw rectangles around the detected faces
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
     # Display the resulting image
     cv2.imshow('Video', frame)
-
     # Exit the loop if the 'q' key is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
